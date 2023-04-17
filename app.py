9be002d9fbde741f0a777c0cdadd138afe1cca86
Bilal Sheikh <65639242+web-desire-creator@users.noreply.github.com>
2023-04-17 13:04:58 +0500

    Final Product made at room

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 import os
 import pandas as pd
 def remove_common(a, b):
- 
     a, b = list(set(a) - set(b)), list(set(b) - set(a))
- 
-    print("list1 : ", a)
-    print("list2 : ", b)
+    print("Additional files in Excel file : ", a)
+    print("Additional Elements in directory : ", b)
 
-dataframe1 = pd.read_excel('Source DG-Setup.xlsx',usecols='C')
-print(dataframe1)
+dataframe1 = pd.read_excel('Source DG-Setup.xlsx')
+# print(dataframe1)
 a=[]
 for i in range(0, len(dataframe1)):
-    a.append(dataframe1["FORMULA"].values[i].split("\\")[1])
+    a.append(dataframe1["FORMULA"].values[i].split("\\")[-1])
 
-print(a)
-
-# folder path
-dir_path = r'D:\\kcp assignments\\image finder'
+dir_path = os.getcwd()
 
 # list to store files
 res = []
@@ -26,5 +21,5 @@ for path in os.listdir(dir_path):
     # check if current path is a file
     if os.path.isfile(os.path.join(dir_path, path)):
         res.append(path)
-print(res)    
+# print(res)    
 remove_common(a, res)
